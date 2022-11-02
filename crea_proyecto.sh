daa83b42007d17a60c8176f027f3657db08f7911
wbarrazaj <wbarraza@gmail.com>
2022-11-02 16:54:18 -0300

    Carga Inicial

---
+++ crea_proyecto.sh
@@ -0,0 +1,17 @@
+
+cd $1
+
+mkdir clases datos diagramas docs logs modulos scripts test
+
+touch __main__.py
+touch __init__.py
+
+touch clases/__init__.py 
+touch datos/__init__.py 
+touch diagramas/__init__.py 
+touch docs/__init__.py 
+touch logs/__init__.py 
+touch modulos/__init__.py 
+touch scripts/__init__.py 
+touch test/__init__.py 
+
