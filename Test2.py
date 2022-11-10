589293695a41e0d7da11d9df6ee5498276f26d67
Wilson Barraza <wbarraza@gmail.com>
2022-11-09 22:08:08 -0300

    .

--- Test2.py
+++ Test2.py
@@ -1,23 +1,32 @@
+#!/usr/bin/env python3
+#_*_ coding: utf-8 _*_
+
 import subprocess
 import sys
+import os
+import datetime
 
-#home_dir = os.system("pwd")
+Comando =  'redis-cli'
+Servidor =  "Aquiles"
+Usuario = "monitor"
+Clave = "Emilita01"
+Puerto = 6379
+Comando_REDIS = "INFO MEMORY"
+Comando_REDIS_Split  = Comando_REDIS.split()
 
-#print(home_dir)
+mensaje = [Comando,"-h", Servidor, "--user",Usuario, "--pass",Clave ,"-p" ,str(Puerto)]
 
+for xx_comando in Comando_REDIS_Split :
+    mensaje.append(xx_comando)
 
-result = subprocess.Popen(["redis-cli", "INFO","MEMORY"], text=True,stdout=subprocess.PIPE)
-
-#print("stdout:", result.stdout)
+result = subprocess.Popen(mensaje,text=True,stdout=subprocess.PIPE)
 
 i=0
-for linea in result.stdout :   
+for linea in result.stdout  :   
     if i>0 :
-        Parametros,Values = linea.rstrip().split(":")
-        print (str(i)+") Paramtros " + Parametros + " : " + Values )
+        Parametros, Valores = linea.rstrip().split(":")
+        print(Parametros, Valores)
         i+=1
     else : 
         i+=1
-
-
 
